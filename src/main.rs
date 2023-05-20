723e6508478bd9d8c3fd2708887ffa947c367ba1
Chris McDearman <mcdearmanchris@gmail.com>
2023-05-20 16:37:11 -0500

    add cli functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use interpreter::repl::repl;
+use std::{cell::RefCell, rc::Rc};
+
+use clap::Parser;
+use interpreter::{eval, repl::repl};
+use parser::ast::Item;
 
 mod compiler;
 mod intern;
@@ -7,6 +11,29 @@ mod list;
 mod parser;
 mod vm;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    filepath: Option<String>,
+
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+}
+
 fn main() {
-    repl();
+    let args = Cli::parse();
+
+    if let Some(filepath) = args.filepath {
+        let src = std::fs::read_to_string(filepath).expect("failed to read file");
+        let env = Rc::new(RefCell::new(interpreter::Env::new()));
+        match &parser::Parser::new(&src).item().expect("failed to parse") {
+            Item::Data(_) => todo!(),
+            Item::Decl(d) => {
+                interpreter::handle_decl(env.clone(), d).expect("failed to handle decl")
+            }
+            Item::Expr(e) => println!("{}", eval(env.clone(), e).expect("failed to eval")),
+        }
+    } else {
+        repl();
+    }
 }
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,124 +1,15 @@
 use crate::{intern::InternedString, list::List};
 use std::{collections::HashMap, fmt::Display};
 
-mod gc;
+use self::{
+    instr::Instr,
+    value::{Struct, Value},
+};
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Value {
-    Int(i64),
-    Float(f64),
-    Bool(bool),
-    String(InternedString),
-    Symbol(InternedString),
-    List(List<Value>),
-    Tuple(Vec<Value>),
-    Map(HashMap<InternedString, Value>),
-    Struct(Struct),
-    Nil,
-}
-
-impl Display for Value {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Value::Int(value) => write!(f, "{}", value),
-            Value::Float(value) => write!(f, "{}", value),
-            Value::Bool(value) => write!(f, "{}", value),
-            Value::String(value) => write!(f, "{}", value),
-            Value::Symbol(value) => write!(f, "{}", value),
-            Value::List(l) => write!(f, "{:?}", l.clone()),
-            Value::Tuple(t) => write!(f, "{:?}", t),
-            Value::Map(m) => write!(f, "{:?}", m),
-            Value::Struct(s) => write!(f, "{}", s),
-            Value::Nil => write!(f, "nil"),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Struct {
-    pub name: InternedString,
-    pub fields: HashMap<InternedString, Value>,
-}
-
-impl Display for Struct {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {{", self.name)?;
-        for (i, (key, value)) in self.fields.iter().enumerate() {
-            if i > 0 {
-                write!(f, ", ")?;
-            }
-            write!(f, "{}: {}", key, value)?;
-        }
-        write!(f, "}}")
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Instr {
-    Load(u16),
-    Store(Value),
-    LoadConst(u16),
-    StoreConst(Value),
-    Push(Value),
-    Pop,
-    Dup,
-    Swap,
-    Drop,
-    Neg,
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Mod,
-    And,
-    Or,
-    Xor,
-    Not,
-    Eq,
-    Neq,
-    Lt,
-    Gt,
-    Jump,
-    Call,
-    Return,
-    Jeq,
-    Halt,
-}
-
-impl Display for Instr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Instr::Load(addr) => write!(f, "LOAD {}", addr),
-            Instr::Store(value) => write!(f, "STORE, {}", value),
-            Instr::LoadConst(addr) => write!(f, "LOADC {}", addr),
-            Instr::StoreConst(value) => write!(f, "STOREC {}", value),
-            Instr::Push(value) => write!(f, "PUSH {}", value),
-            Instr::Pop => write!(f, "POP"),
-            Instr::Dup => write!(f, "DUP"),
-            Instr::Swap => write!(f, "SWAP"),
-            Instr::Drop => write!(f, "DROP"),
-            Instr::Neg => write!(f, "NEG"),
-            Instr::Add => write!(f, "ADD"),
-            Instr::Sub => write!(f, "SUB"),
-            Instr::Mul => write!(f, "MUL"),
-            Instr::Div => write!(f, "DIV"),
-            Instr::Mod => write!(f, "MOD"),
-            Instr::And => write!(f, "AND"),
-            Instr::Or => write!(f, "OR"),
-            Instr::Xor => write!(f, "XOR"),
-            Instr::Not => write!(f, "NOT"),
-            Instr::Eq => write!(f, "EQ"),
-            Instr::Neq => write!(f, "NEQ"),
-            Instr::Lt => write!(f, "LT"),
-            Instr::Gt => write!(f, "GT"),
-            Instr::Jump => write!(f, "JMP"),
-            Instr::Call => write!(f, "CALL"),
-            Instr::Return => write!(f, "RET"),
-            Instr::Jeq => write!(f, "JEQ"),
-            Instr::Halt => write!(f, "HALT"),
-        }
-    }
-}
+pub mod chunk;
+pub mod gc;
+pub mod instr;
+pub mod value;
 
 pub struct VM {
     pc: usize,
