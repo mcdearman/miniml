28bae3b6a5f1e0df671e4b01d1f442617e7698d0
Chris McDearman <mcdearmanchris@gmail.com>
2023-05-22 20:47:26 -0500

    strart rewrite

--- src/parser/cst.rs
+++ src/parser/cst.rs
@@ -1,129 +1,43 @@
 use crate::intern::InternedString;
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum ParseTree {
-    Item(Item),
+pub struct Node {
+    pub kind: SyntaxKind,
+    pub children: Vec<NodeOrToken>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Item {
-    Data(Data),
-    Decl(Decl),
-    Expr(Expr),
+pub enum SyntaxKind {
+    Error,
+    Unit,
+    Int,
+    Bool,
+    Ident,
+    Lambda,
+    Lit,
+    If,
+    Let,
+    Atom,
+    Apply,
+    Unary,
+    Power,
+    Factor,
+    Term,
+    Cmp,
+    Eq,
+    And,
+    Or,
+    Expr,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Data {
-    pub name: InternedString,
-    pub fields: Vec<InternedString>,
+pub struct Token {
+    pub kind: SyntaxKind,
+    pub text: InternedString,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Decl {
-    Fn(FnDecl),
-    Let(LetDecl),
+pub enum NodeOrToken {
+    Node(Node),
+    Token(Token),
 }
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct FnDecl {
-    pub name: InternedString,
-    pub params: Vec<InternedString>,
-    pub body: Expr,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct LetDecl {
-    pub name: InternedString,
-    pub value: Expr,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Expr {
-    pub or: Or,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Or {
-    pub and: And,
-    pub or: Option<Box<Or>>,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct And {
-    pub comp: Eq,
-    pub and: Option<Box<And>>,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Eq {
-    pub comp: Cmp,
-    pub eq: Option<Box<Eq>>,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Cmp {
-    pub add: Term,
-    pub cmp: Option<(CmpOp, Box<Cmp>)>,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum CmpOp {
-    Neq,
-    Lt,
-    Gt,
-    Lte,
-    Gte,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Term {
-    pub factor: Factor,
-    pub term: Option<(TermOp, Box<Term>)>,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum TermOp {
-    Add,
-    Sub,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Factor {
-    pub unary: Power,
-    pub factor: Option<(FactorOp, Box<Factor>)>,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum FactorOp {
-    Mul,
-    Div,
-    Mod,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Power {
-    pub atom: Unary,
-    pub power: Option<Box<Power>>,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Unary {
-    Neg(Box<Unary>),
-    Not(Box<Unary>),
-    Apply(Apply),
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum UnaryOp {
-    Neg,
-    Not,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Apply {
-    pub atom: Atom,
-    pub arg: Atom,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Atom {}
