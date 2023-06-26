b5aa227a0d340a1fad7572868fa93e4181400dd8
Chris McDearman <mcdearmanchris@gmail.com>
2023-06-25 19:26:10 -0500

    start rewrite

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,14 +1,10 @@
-use self::{ast::Item, error::Error, token::Token};
-use crate::parser::parse::parser;
-use chumsky::{input::Stream, prelude::Input, span::SimpleSpan, Parser};
+use self::{ast::Item, error::Error};
 use itertools::Itertools;
-use logos::{Logos, Span};
 
 pub mod ast;
 pub mod cst;
 pub mod error;
 pub mod lex;
-pub mod parse;
 pub mod span;
 mod tests;
 pub mod token;
@@ -26,20 +22,5 @@ pub fn parse(src: &str) -> (Option<Item>, Vec<Error>) {
             .collect_vec(),
     );
 
-    let tok_stream =
-        Stream::from_iter(tokens.into_iter()).spanned(SimpleSpan::from(src.len()..src.len()));
-
-    let res = parser().parse(tok_stream).into_result().map_err(|errs| {
-        errs.into_iter().map(|e| Error {
-            message: format!("{:?}", e),
-        })
-    });
-
-    match res {
-        Ok(item) => (Some(item), errors),
-        Err(errs) => {
-            errors.append(&mut errs.collect_vec());
-            (None, errors)
-        }
-    }
+    todo!()
 }
