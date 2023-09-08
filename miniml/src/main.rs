30aa1aec5860b3db134977ae923c5121eb3b0281
Chris McDearman <mcdearmanchris@gmail.com>
2023-09-07 21:36:51 -0500

    redo main

--- miniml/src/main.rs
+++ miniml/src/main.rs
@@ -12,44 +12,26 @@ struct Cli {
 
 fn main() {
     env_logger::init();
-    // let args = Cli::parse();
-    // let env = Env::new();
+    let args = Cli::parse();
+    let env = Env::new();
 
-    // if let Some(filepath) = args.filepath {
-    //     let src = std::fs::read_to_string(filepath).expect("failed to read file");
-    //     let mut parser = miniml_syntax::parser::Parser::new(&src);
-    //     match parser.repl_parse() {
-    //         (root, errors) => {
-    //             if !errors.is_empty() {
-    //                 println!("errors: {:?}", errors);
-    //             } else {
-    //                 println!("root: {:?}", root);
-    //             }
-    //         }
-    //     }
-    // } else {
-    //     repl();
-    // }
-}
-
-fn ack_memo(m: i64, n: i64) -> i64 {
-    let mut memo = vec![vec![0; n as usize + 1]; m as usize + 1];
-    fn ack(m: i64, n: i64, memo: &mut Vec<Vec<i64>>) -> i64 {
-        if m == 0 {
-            return n + 1;
+    if let Some(filepath) = args.filepath {
+        let src = std::fs::read_to_string(filepath).expect("failed to read file");
+        let mut parser = miniml_syntax::parser::Parser::new(&src);
+        match parser.repl_parse() {
+            (root, errors) => {
+                if !errors.is_empty() {
+                    println!("errors: {:?}", errors);
+                } else {
+                    println!("root: {:?}", root);
+                }
+            }
         }
-        if n == 0 {
-            return ack(m - 1, 1, memo);
-        }
-        if memo[m as usize][n as usize] != 0 {
-            return memo[m as usize][n as usize];
-        }
-        let res = ack(m - 1, ack(m, n - 1, memo), memo);
-        memo[m as usize][n as usize] = res;
-        res
+    } else {
+        repl();
     }
-    ack(m, n, &mut memo)
 }
+
 
 // use logos::Logos;
 // use miniml_syntax::{
