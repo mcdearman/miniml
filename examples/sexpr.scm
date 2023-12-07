90e14c72625ce60e55ed5167bf9750ab7a63825c
mcdearman <mcdearmanchris@gmail.com>
2023-12-06 20:09:17 -0600

    update

---
+++ examples/sexpr.scm
@@ -0,0 +1,15 @@
+;; this is a comment
+ 
+(+ 1 2)
+
+;; class def
+(class span (start end))
+
+;; class method
+(method span 
+
+;; instance method
+(method span (length) (- end start))
+
+;; instance
+(span 1 2)
