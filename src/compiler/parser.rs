3b45c703ffa8917998452ae0296a34c465209305
mcdearman <mcdearmanchris@gmail.com>
2023-12-14 05:06:22 -0600

    update

--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -1,15 +1,6 @@
+use super::{ast, node::Node};
 use std::fmt::Debug;
 
 pub trait Parser: Debug + Clone {
-    // fn parse(&self) -> Node;
+    fn parse(&self) -> Node<ast::Root>;
 }
-
-// pub trait Lexer<T: Token>: Debug + Clone + Iterator<Item = T> {
-//     fn next(&self) -> T;
-// }
-
-// pub trait Token: Debug + Clone {
-//     fn get_type(&self) -> TokenType;
-// }
-
-// pub trait TokenType: Debug + Clone {}
