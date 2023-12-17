9259a629cc2173de5ba6a1aac07cc3d0f2523ad4
mcdearman <mcdearmanchris@gmail.com>
2023-12-16 23:53:50 -0600

    update

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,6 @@ mod token;
 
 fn main() {
     env_logger::init();
-    // let src = "0xff";
-    // // let num = num::Num::from_str(src).unwrap();
-    // let num = Rational64::from_str_radix(&src[2..], 16).expect("failed to parse");
-    // // let num = i64::from_str_radix(&src[2..], 16).expect("failed to parse");
-    // println!("{:?}", num);
 
     // open examples/notebook.mn
     let src = std::fs::read_to_string("examples/notebook.mn").unwrap();
