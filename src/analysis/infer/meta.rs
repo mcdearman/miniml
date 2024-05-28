fd433bc34a7d98d919e07d3fa73130412703a0ac
mcdearman <mcdearmanchris@gmail.com>
2024-05-27 22:05:08 -0500

    update

--- src/analysis/infer/meta.rs
+++ src/analysis/infer/meta.rs
@@ -25,7 +25,7 @@ impl Meta {
 
 impl Debug for Meta {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "v{}", self.0)
+        write!(f, "{}", self)
     }
 }
 
