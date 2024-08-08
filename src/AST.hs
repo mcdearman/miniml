d88193fc8bf18a6f0fe2a1fe0e9296883ec7c467
mcdearman <mcdearmanchris@gmail.com>
2024-08-08 00:44:23 -0500

    update

--- src/AST.hs
+++ src/AST.hs
@@ -1,14 +1,23 @@
+{-# LANGUAGE GADTs #-}
+
 module AST where
 
-data Def = Def String Expr deriving (Show)
+data Def a = Def String (Expr a)
 
-data Expr
-  = ELit Lit
-  | EVar String
-  | EApp Expr Expr
-  | ELam String Expr
-  | ELet String Expr Expr
-  deriving (Show)
+-- data Expr
+--   = ELit Lit
+--   | EVar String
+--   | EApp Expr Expr
+--   | ELam String Expr
+--   | ELet String Expr Expr
+--   deriving (Show)
+
+data Expr a where
+  I :: Int -> Expr Int
+  B :: Bool -> Expr Bool
+  Add :: Expr Int -> Expr Int -> Expr Int
+  Mul :: Expr Int -> Expr Int -> Expr Int
+  Eq :: (Eq a) => Expr a -> Expr a -> Expr Bool
 
 data Lit
   = LInt Integer
