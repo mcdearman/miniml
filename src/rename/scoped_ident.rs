cb8273496f3db3fb4f69296201adf4cfa66f1e06
mcdearman <mcdearmanchris@gmail.com>
2024-08-12 14:13:48 -0500

    update

--- src/rename/scoped_ident.rs
+++ src/rename/scoped_ident.rs
@@ -1,20 +1,39 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 use crate::utils::{intern::InternedString, span::Span};
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy)]
 pub struct ScopedIdent {
+    pub id: usize,
     pub name: InternedString,
-    pub level: usize,
     pub span: Span,
 }
 
 impl ScopedIdent {
-    pub fn new(name: InternedString, level: usize, span: Span) -> Self {
-        Self { name, level, span }
+    pub fn new(id: usize, name: InternedString, span: Span) -> Self {
+        Self { id, name, span }
     }
 }
 
 impl std::fmt::Display for ScopedIdent {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}{}", self.name, self.level)
+        write!(f, "{}{}", self.name, self.id)
+    }
+}
+
+static COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+impl ScopedIdent {
+    pub fn gen() -> Self {
+        Self {
+            id: COUNTER.fetch_add(1, Ordering::SeqCst),
+
+        }
+    }
+}
+
+impl PartialEq for ScopedIdent {
+    fn eq(&self, other: &ScopedIdent) -> bool {
+        self.id == other.id
     }
 }
