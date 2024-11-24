f460772bbb8baad3a91b814b4611b376a1844afe
mcdearman <mcdearmanchris@gmail.com>
2024-11-24 09:56:04 -0600

    update

--- miniml-analysis/src/error.rs
+++ miniml-analysis/src/error.rs
@@ -1,4 +1,4 @@
-use crate::utils::{intern::InternedString, span::Span};
+use miniml_utils::{intern::InternedString, span::Span};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct AnalysisError {
--- miniml-analysis/src/lib.rs
+++ miniml-analysis/src/lib.rs
@@ -1,6 +1,5 @@
-pub mod core;
 pub mod error;
-pub mod exhaustivity;
-pub mod infer;
+// pub mod exhaustivity;
+// pub mod infer;
 pub mod scc;
 // pub mod mir_lower;
