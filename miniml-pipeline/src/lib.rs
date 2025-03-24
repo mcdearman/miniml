570a5d849d80eb0e5500e70e7101d6f618c1497e
mcdearman <mcdearmanchris@gmail.com>
2025-03-23 23:57:58 -0500

    update

--- miniml-pipeline/src/lib.rs
+++ miniml-pipeline/src/lib.rs
@@ -8,12 +8,8 @@ impl<'src> Pipeline<'src> {
         Self { src }
     }
 
-    pub fn run(&self) -> Result<(), Error> {
+    pub fn run(&self) {
         let stream = TokenStream::new(self.src);
-        let (ast, _) = parse(stream, true);
-        let ast = ast.ok_or_else(|| Error::ParseError)?;
-        let ast = rename(ast)?;
-        let ast = infer(ast)?;
-        Ok(())
+        let (ast, errors) = parse(stream, true);
     }
 }
