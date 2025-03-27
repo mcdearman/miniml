7ac6df340b6ca9d034894f2571ca837c36057c8e
mcdearman <mcdearmanchris@gmail.com>
2025-03-26 23:10:41 -0500

    move compiler to it's own directory and add lib dir

---
+++ compiler/mmc-ast/src/token_stream.rs
@@ -0,0 +1,27 @@
+use super::token::Token;
+use logos::{Lexer, Logos};
+use mmc_utils::span::Span;
+
+#[derive(Debug, Clone)]
+pub struct TokenStream<'src> {
+    logos: Lexer<'src, Token>,
+}
+
+impl<'src> TokenStream<'src> {
+    pub fn new(src: &'src str) -> Self {
+        Self {
+            logos: Token::lexer(src),
+        }
+    }
+}
+
+impl<'src> Iterator for TokenStream<'src> {
+    type Item = (Token, Span);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.logos.next().map(|res| match res {
+            Ok(t) => (t, Span::from(self.logos.span())),
+            Err(_) => (Token::Error, Span::from(self.logos.span())),
+        })
+    }
+}
---
+++ compiler/mmc/src/main.rs
@@ -0,0 +1,68 @@
+use mmc_pipeline::Pipeline;
+use rustyline::{
+    error::ReadlineError, validate::Validator, Completer, Editor, Helper, Highlighter, Hinter,
+};
+
+#[derive(Completer, Helper, Highlighter, Hinter)]
+struct TermValidator;
+
+impl Validator for TermValidator {
+    fn validate(
+        &self,
+        ctx: &mut rustyline::validate::ValidationContext,
+    ) -> rustyline::Result<rustyline::validate::ValidationResult> {
+        if ctx.input().ends_with("\n") {
+            Ok(rustyline::validate::ValidationResult::Valid(None))
+        } else {
+            Ok(rustyline::validate::ValidationResult::Incomplete)
+        }
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let h = TermValidator;
+    let mut rl = Editor::new().expect("Failed to create editor");
+    rl.set_helper(Some(h));
+    if rl.load_history(".miniml_history").is_err() {
+        eprintln!("No previous history.");
+    }
+
+    println!("Welcome to MiniML!");
+
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                match line.trim() {
+                    ":q" | ":quit" => break,
+                    "clear" => {
+                        rl.clear_history().expect("history failed to clear");
+                        continue;
+                    }
+                    _ => (),
+                }
+                rl.add_history_entry(line.as_str())
+                    .expect("Failed to add history entry");
+
+                let pipeline = Pipeline::new(&line);
+                pipeline.run();
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history(".miniml_history")
+        .expect("Failed to save history");
+}
