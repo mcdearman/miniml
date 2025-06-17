67989b6345448025a110cd9e4d44fa042088f15f
mcdearman <mcdearmanchris@gmail.com>
2025-06-17 01:25:37 -0500

    start implementing layout rule

--- compiler/MMC/Layout.hs
+++ compiler/MMC/Layout.hs
@@ -1,12 +1,14 @@
 module Layout where
 
 import Data.Text (Text)
-import MMC.Token (LToken)
-
-type IndentLevel = Int
+import MMC.Common (Loc (..), Located (..))
+import MMC.Token (LToken, Token (TokLBrace))
 
 runLayout :: Text -> [LToken] -> [LToken]
 runLayout src tokens = undefined
 
-offsideRule :: [LToken] -> [LToken]
-offsideRule tokens = undefined
+layout :: [LToken] -> [Int] -> [LToken]
+
+layout (t : ts) ms = t : layout ts ms
+layout [] [] = []
+layout [] (m : ms) = Located TokLBrace (Loc 0 0) : layout [] ms
