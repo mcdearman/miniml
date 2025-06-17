0ed3e8d6141af621a5512291c242da51d18e6811
mcdearman <mcdearmanchris@gmail.com>
2025-06-17 00:57:49 -0500

    update

--- compiler/MMC/Layout.hs
+++ compiler/MMC/Layout.hs
@@ -3,5 +3,10 @@ module Layout where
 import Data.Text (Text)
 import MMC.Token (LToken)
 
+type IndentLevel = Int
+
 runLayout :: Text -> [LToken] -> [LToken]
 runLayout src tokens = undefined
+
+offsideRule :: [LToken] -> [LToken]
+offsideRule tokens = undefined
