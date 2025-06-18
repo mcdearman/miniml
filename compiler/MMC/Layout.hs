6954f077d713b72f958984484c79ff908b55cf2b
mcdearman <mcdearmanchris@gmail.com>
2025-06-17 23:22:17 -0500

    update

--- compiler/MMC/Layout.hs
+++ compiler/MMC/Layout.hs
@@ -2,13 +2,15 @@ module Layout where
 
 import Data.Text (Text)
 import MMC.Common (Loc (..), Located (..))
-import MMC.Token (LToken, Token (TokLBrace))
+import MMC.Token (LToken, Token (..))
 
 runLayout :: Text -> [LToken] -> [LToken]
 runLayout src tokens = undefined
 
 layout :: [LToken] -> [Int] -> [LToken]
-
+layout (Located (TokWhitespace n) l : ts) (m : ms)
+  | m == n = Located TokSemi l : layout ts (m : ms)
+  | n < m = Located TokLBrace l : layout (Located (TokWhitespace n) l : ts) ms
 layout (t : ts) ms = t : layout ts ms
 layout [] [] = []
-layout [] (m : ms) = Located TokLBrace (Loc 0 0) : layout [] ms
+layout [] (m : ms) | m /= 0 = Located TokLBrace (Loc 0 0) : layout [] ms
