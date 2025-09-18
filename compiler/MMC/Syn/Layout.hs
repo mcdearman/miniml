fd2e2b86aaf9bed525f4df71d39963e2187cba4e
mcdearman <mcdearmanchris@gmail.com>
2025-09-18 05:58:14 -0500

    update

--- compiler/MMC/Syn/Layout.hs
+++ compiler/MMC/Syn/Layout.hs
@@ -31,48 +31,49 @@ data VTok
 --      y = 2
 --  in x + y
 
-data Cursor = Cursor
-  { tokens :: [Token],
-    stack :: [Frame],
-    offset :: !Int,
-    col :: !Int
-  }
+-- data Cursor = Cursor
+--   { tokens :: [Token],
+--     stack :: [Frame],
+--     offset :: !Int,
+--     col :: !Int
+--   }
 
-data Frame = Braced | Layout Int deriving (Show, Eq)
+-- type LayoutM = State Cursor
 
-type LayoutM = State Cursor
+-- runLayout :: [Token] -> PipelineM [VTok]
+-- runLayout ts = do
+--   li <- asks pipelineLineIndex
+--   pure $ evalState (layout li) (Cursor ts [] 0 1)
 
-runLayout :: [Token] -> PipelineM [VTok]
-runLayout ts = do
-  li <- asks pipelineLineIndex
-  pure $ evalState (layout li) (Cursor ts [] 0 1)
+-- layout :: LineIndex -> LayoutM [VTok]
+-- layout li = undefined
 
-layout :: LineIndex -> LayoutM [VTok]
-layout li = undefined
+-- bump :: LineIndex -> LayoutM VTok
+-- bump li = do
+--   Cursor ts stack offset col <- get
+--   case ts of
+--     [] -> error "no more tokens"
+--     (t : ts') -> do
+--       put $ Cursor ts' stack (offset + tokenLength t) (col + 1)
+--       pure $ VTok t
 
-bump :: LineIndex -> LayoutM VTok
-bump li = do
-  Cursor ts stack offset col <- get
-  case ts of
-    [] -> error "no more tokens"
-    (t : ts') -> do
-      put $ Cursor ts' stack (offset + tokenLength t) (col + 1)
-      pure $ VTok t
+layout :: LineIndex -> [Token] -> [VTok]
+layout li ts = go li 0 ts []
+  where
+    go :: LineIndex -> Int -> [Token] -> [Int] -> [VTok]
+    --  L ({n} : ts) [] = {  :  (L ts [n]) if n > 0 (Note 1)
+    go li offset (t : c : ts) []
+      | isLayoutHerald t c =
+          let (_, col) = offsetToLineCol li offset
+           in VIndent col : go li col (c : ts) [col]
+    go li col (t : ts) (0 : ms) | tokenKind t == SyntaxKindRBrace = VTok t : go li col ts ms
+    go _ _ (t : ts) ms | tokenKind t == SyntaxKindRBrace = error "unmatched right brace"
+    go li o (t : ts) ms | tokenKind t == SyntaxKindLBrace = VTok t : go li o ts (0 : ms)
+    go li o (t : ts) (m : ms) | m /= 0 = VDedent m : go li o (t : ts) ms
+    go li o (t : ts) ms = VTok t : go li o ts ms
+    go _ _ [] [] = []
+    go li o [] (m : ms) | m /= 0 = VDedent m : go li o [] ms
+    go _ _ _ _ = undefined
 
--- layout :: LineIndex -> Int -> [Token] -> [Int] -> [VTok]
--- --  L ({n} : ts) [] = {  :  (L ts [n]) if n > 0 (Note 1)
--- layout li offset (t : c : ts) []
---   | isLayoutHerald t c =
---       let (_, col) = offsetToLineCol li offset
---        in VIndent col : layout li col (c : ts) [col]
--- layout li col (t : ts) (0 : ms) | tokenKind t == SyntaxKindRBrace = VTok t : layout li ts ms
--- layout _ _ (t : ts) ms | tokenKind t == SyntaxKindRBrace = error "unmatched right brace"
--- layout li _ (t : ts) ms | tokenKind t == SyntaxKindLBrace = VTok t : layout li ts (0 : ms)
--- layout li _ (t : ts) (m : ms) | m /= 0 = VDedent m : layout li (t : ts) ms
--- layout li _ (t : ts) ms = VTok t : layout li ts ms
--- layout _ _ [] [] = []
--- layout li _ [] (m : ms) | m /= 0 = VDedent m : layout li [] ms
-
--- layout _ _ _ = undefined
 isLayoutHerald :: Token -> Token -> Bool
 isLayoutHerald t c = isLayoutKeyword t && tokenKind c == SyntaxKindColon
